c5a7c497c52ae105cdeb442ba5ec6dd6ad8b6468
Nami <jobwave2019@gmail.com>
2017-05-29 04:39:20 +0900

    BGMのループ再生可能

--- Resouces/Music/CueSheet_0.h
+++ Resouces/Music/CueSheet_0.h
@@ -4,8 +4,8 @@
  *  Tool Ver.        : CRI Atom Craft LE Ver.2.19.02
  *  File Path        : C:/Users/s162098/Documents/CriAtomCraft/OrionMusic/Public/WorkUnit_0/CueSheet_0.acb
  *  File Name        : CueSheet_0.acb
- *  File Size        : 49,120 bytes
- *  Date Time        : 2017/05/29 2:02
+ *  File Size        : 49,248 bytes
+ *  Date Time        : 2017/05/29 4:34
  *  Target           : Public
  *  Cues             : 8
  *  CueSheet Comment : 
@@ -19,14 +19,14 @@
 
 
 /* Cue List (Cue ID) */
-#define CRI_CUESHEET_0_FLY_MOVE  ( 0)	//プレイヤーの移動SE
-#define CRI_CUESHEET_0_HIT       ( 1)	//欠片とワイヤーが当たったSE
-#define CRI_CUESHEET_0_PLAY      ( 2)	//プレイシーンBGM
-#define CRI_CUESHEET_0_PUSH_KEY  ( 3)	//キーが押されたSE
-#define CRI_CUESHEET_0_RESULT    ( 4)	//リザルト(オーバーシーン)BGM
-#define CRI_CUESHEET_0_THROW     ( 5)	//ワイヤーを投げたSE
-#define CRI_CUESHEET_0_TITLE     ( 6)	//タイトルシーンBGM
-#define CRI_CUESHEET_0_VANISH    ( 7)	//欠片と針が当たって欠片が消えるSE
+#define CRI_CUESHEET_0_FLY_MOVE  ( 0) /*   */
+#define CRI_CUESHEET_0_HIT       ( 1) /*   */
+#define CRI_CUESHEET_0_PLAY      ( 2) /*   */
+#define CRI_CUESHEET_0_PUSH_KEY  ( 3) /*   */
+#define CRI_CUESHEET_0_RESULT    ( 4) /*   */
+#define CRI_CUESHEET_0_THROW     ( 5) /*   */
+#define CRI_CUESHEET_0_TITLE     ( 6) /*   */
+#define CRI_CUESHEET_0_VANISH    ( 7) /*   */
 
 /* Block List (Block Index) */
 
